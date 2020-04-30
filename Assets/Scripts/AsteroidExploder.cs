b8153c12ad5ad0bf1a7917c1a117380427556253
skyblue fox <xremberx@gmail.com>
2020-04-30 16:33:24 +0300

    added asteroids

---
+++ Assets/Scripts/AsteroidExploder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidExploder : MonoBehaviour
+{
+    public float destroyTimer = 1.0f;
+
+    private List<Collider> childrenColliders;
+
+    private void Start()
+    {
+        childrenColliders = new List<Collider>();
+
+        foreach (var collider in GetComponentsInChildren<Collider>())
+        {
+            collider.enabled = false;
+            childrenColliders.Add(collider);
+        }
+        childrenColliders[0].enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        
+        Destroy(this.gameObject, destroyTimer);
+        foreach (var collider in GetComponentsInChildren<Collider>())
+            collider.enabled = true;
+    }
+}
