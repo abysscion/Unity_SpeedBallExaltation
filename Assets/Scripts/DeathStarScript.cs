a9c488215abb313dc972bbbd712f09493fb9a355
skyblue fox <xremberx@gmail.com>
2020-05-02 16:00:52 +0300

    added red star, added metallic meteor plus both scripts

---
+++ Assets/Scripts/DeathStarScript.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class DeathStarScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            GameController.currentGameState = GameController.GameState.Lose;
+        }
+    }
+}
---
+++ Assets/Scripts/MetallicAsteroidCollisionScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MetallicAsteroidCollisionScript : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.transform.CompareTag("Player")) return;
+        
+        var jumper = other.gameObject.GetComponent<JumpController>();
+        var jumperRb = jumper.GetComponent<Rigidbody>();
+
+        if (jumper.isStick)
+        {
+            jumper.UnstickBall();
+            jumperRb.velocity = Vector3.down * 6;
+        }
+        else
+        {
+            jumperRb.velocity = Vector3.down * 4;
+        }
+    }
+}
