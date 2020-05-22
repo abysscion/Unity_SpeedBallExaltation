a486b13dafd25f0f634e3da5b429c614caa37940
Zabilya <36747171+Zabilya@users.noreply.github.com>
2020-05-22 15:56:55 +0300

    add coins and zablochil perevorot screen'a

---
+++ Assets/Scripts/CoinsScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Controllers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsScript : MonoBehaviour
+{
+    private Vector3 _coinsTextPosition;
+    private GameObject _coinsText;
+    private bool _isAlreadyDestroing = false;
+    private Vector3 _direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coinsText = GameObject.Find("CoinsAmountText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > _coinsText.transform.position.y)
+            Destroy(this.gameObject);
+        if (!_isAlreadyDestroing)
+        {
+            _isAlreadyDestroing = !_isAlreadyDestroing;
+            _direction = _coinsText.GetComponent<RectTransform>().position - GetComponent<RectTransform>().position;
+        }
+        
+        transform.Translate(_direction * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        GameController.Instance.AddCoins(1, _coinsText.GetComponent<Text>());
+    }
+}
--- Assets/Scripts/RewardBoxOpenScript.cs
+++ Assets/Scripts/RewardBoxOpenScript.cs
@@ -92,8 +92,19 @@ public class RewardBoxOpenScript : MonoBehaviour
     public void TossCoinsAnimation()
     {
         //TODO: ILYA WILL MAKE AN ANIMATION HERE!
-        GameController.Instance.AddCoins(Random.Range(RewardMin, RewardMax), _coinsAmountText);
-        Debug.Log("Coins tossed!");
+        int coinsAmount = Random.Range(RewardMin, RewardMax);
+        // GameController.Instance.AddCoins(coinsAmount, _coinsAmountText);
+        GameObject canvas = GameObject.Find("UI");
+        GameObject coin = Resources.Load<GameObject>("Prefabs/Coin2d");
+        for (int i = 0; i < coinsAmount; i++)
+        {
+            //TODO монетки должны вылетать из метеорита, а не из центра экрана
+            Vector3 pos = new Vector3(Random.Range(transform.position.x - 100.0f, transform.position.x + 100.0f), 
+                Random.Range(transform.position.y - 100.0f, transform.position.y + 100.0f), transform.position.z);
+            coin = Instantiate(coin, pos, Quaternion.identity);
+            coin.transform.SetParent(canvas.transform, false);
+        }
+        // Debug.Log("Coins tossed!");
         StartCoroutine(nameof(GoToNextLevel));
     }
 
